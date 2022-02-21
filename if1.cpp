99df655d76d991da4f8e6e5d9bbdead723970b35
wasimk739 <wasimk739@gmail.com>
2022-02-22 01:24:16 +0530

    Write a C++ program to compute the sum of the two given integer values. If the two values are the same, then return triple their sum.

---
+++ if1.cpp
@@ -0,0 +1,19 @@
+// Write a C++ program to compute the sum of the two given integer values. If the two values are the same, then return triple their sum.
+
+#include <iostream>
+
+using namespace std;
+
+int test(int x, int y)
+        {
+            return x == y ? (x + y)*3 : x + y;
+        }
+        
+        
+int main() 
+ {
+  cout << test(1, 2) << endl;  
+  cout << test(3, 2) << endl;  
+  cout << test(2, 2) << endl;  
+  return 0;    
+}
