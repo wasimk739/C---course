6f6bf3864c0f1fed7fffd95430f6eea81293666d
wasimk739 <wasimk739@gmail.com>
2022-02-25 15:31:36 +0530

    Write a C++ program to remove the character in a given position of a given string. The given position will be in the range 0..string length -1 inclusive.

---
+++ stringoper.cpp
@@ -0,0 +1,18 @@
+// Write a C++ program to remove the character in a given position of a given string. The given position will be in the range 0..string length -1 inclusive.
+
+#include <iostream>
+
+using namespace std;
+
+string test(string str, int n)
+        {
+            return str.erase(n, 1);
+        }
+        
+int main() 
+ {
+  cout << test("Python", 1) << endl;  
+  cout << test("Python", 0) << endl;  
+  cout << test("Python", 4) << endl;  
+  return 0;    
+}
