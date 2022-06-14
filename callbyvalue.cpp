f7f15d80be81947f57aceaf0e9cacdd4be1bb6ef
wasimk739 <wasimk739@gmail.com>
2022-06-14 11:27:51 +0530

    updated

--- callbyvalue.cpp
+++ callbyvalue.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int test(int formal){   //formal is a copy of actual
-    cout<<formal<<endl;   //50
+    // cout<<formal<<endl;   //50
     formal=100;            //only changes the local copy
     cout<<formal;          //100
     return 0;
@@ -11,8 +11,8 @@ int test(int formal){   //formal is a copy of actual
 
 int main(){
     int actual=50;
-    cout<<actual<<endl;        //50
+    // cout<<actual<<endl;        //50
     test(actual);             //pass in 50 to test
-    cout<<endl<<actual<<endl;        //50 didn't change
+    // cout<<endl<<actual<<endl;        //50 didn't change
     return 0;   
 }
